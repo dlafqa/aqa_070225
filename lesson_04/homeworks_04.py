8dff34ed20b13e08237ae807e90a135226fab248
dl.danikua <dl.danikua@gmail.com>
2025-03-07 14:12:03 +0200

    homework4

--- lesson_04/homeworks_04.py
+++ lesson_04/homeworks_04.py
@@ -21,49 +21,69 @@ rolling in wealth."""
 # task 01 ==
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
-
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("\n", " ")
 # task 02 ==
 """ Замініть .... на пробіл
 """
-
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
 
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
-
+words = adwentures_of_tom_sawer.split()
+adwentures_of_tom_sawer = " ".join(words)
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
 """
-adwentures_of_tom_sawer.count("h")
+h_count = adwentures_of_tom_sawer.count("h")
+print(h_count)
 
 # task 05
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 """
-
+words = adwentures_of_tom_sawer.split()
+count = 0
+for word in words:
+    if word[0].isupper():
+        count = count + 1
+print(count)
 
 # task 06
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
-
+first = adwentures_of_tom_sawer.find("Tom")
+second = adwentures_of_tom_sawer.find("Tom", first + 1)
+print(second)
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
 Збережіть результат у змінній adwentures_of_tom_sawer_sentences
 """
-adwentures_of_tom_sawer_sentences = None
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split(". ")
+for i in range(len(adwentures_of_tom_sawer_sentences) - 1):
+    adwentures_of_tom_sawer_sentences[i] = adwentures_of_tom_sawer_sentences[i] + "."
 
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
-
+fourth = adwentures_of_tom_sawer_sentences[3]
+fourth_lower = fourth.lower()
+print(fourth_lower)
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
-
+found = False
+for sentence in adwentures_of_tom_sawer_sentences:
+    if sentence.startswith("By the time"):
+        found = True
+print(found)
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+last_sentence = adwentures_of_tom_sawer_sentences[-1]
+word_count = len(last_sentence.split())
+print(word_count)
